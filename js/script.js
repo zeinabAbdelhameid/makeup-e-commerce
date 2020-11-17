9a2e70f2540d63d4d8cb686ea7b954e306105f27
zeinabAbdelhameid <zm8131732@gmail.com>
2020-11-17 20:41:00 +0200

    four commit

--- js/script.js
+++ js/script.js
@@ -1,23 +1,13 @@
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementsByClassName('navbar');
-var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
-$('.slider-area').slick({
+$('.video').slick({
 	dots: true,
 	autoplay: true,
 	 infinite: true,
   speed: 2000,
   fade: true,
-  
+   prevArrow: ' ',
+  nextArrow: ' ',	
 });
 $('.end').slick({
 	dots: true,
@@ -26,51 +16,6 @@ $('.end').slick({
 	autoPlay: true,
 });
 
-$('.mac-brand').slick({
- Infinit: true,	
-dots: true,	
-  centerMode: true,
- slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-	 lazyLoad: 'ondemand',
-	
-	 
-	
-});
-$(document).ready(function(){
-  $("#here").click(function(){
-	  $("#here").animate({fontSize: '3em'});
-					   
-	  
-  });
-	});
-$('.essence-brand').slick({
-  centerMode: true,
-  dots: true,
-  slidesToShow: 3,
-  autoplay: true,
-  autoplaySpeed: 5000,	
-  
-});
-$('.catrice-brand').slick({
-Infinit: true,	
-dots: true,	
-  centerMode: true,
- slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-	 lazyLoad: 'ondemand',
 
-});
-$(document).ready(function(){
-	$(document).on("click",".fas",function(){
-		
-	
-		$(this).toggleClass("fav");
-	
-		
-	});
-});
+	  
+ 
